54c30d2e7bd00f73d422794046863252c1263797
rolandm99 <manfulmweze99@gmail.com>
2022-04-22 14:01:14 +0100

    Create rails app

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+greeting1 = Greeting.create(name:'Good morning')
+greeting2 = Greeting.create(name:'Hello team')
+greeting3 = Greeting.create(name:'Hello Microverse')
+
